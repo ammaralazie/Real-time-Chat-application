ee204d0b9d9ff7ed47f61227a41e8a0afbbd4e3d
ammaralazii <alaziiammar@gmail.com>
2021-07-04 17:32:31 +0300

    code

--- app/Http/Controllers/ChatApp/ChatsController.php
+++ app/Http/Controllers/ChatApp/ChatsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\ChatApp;
 
 use App\Http\Controllers\Controller;
 use App\Message;
+use App\ReciveMessage;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -15,6 +16,7 @@ class ChatsController extends Controller
 
         $authUser = Auth::user();
 
+        //take my id which found in table create_recive_messages_table
         $users = User::whereHas('reciveUser', function ($q) use ($authUser) {
             return $q->where('user_id', $authUser->id);
         })->get();
@@ -25,23 +27,56 @@ class ChatsController extends Controller
             $sndUsr[$item->id] = User::findOrFail($msg->user_id);
         } //end of foreach
 
+
         foreach ($sndUsr as $key => $item) {
             foreach ($sndUsr as $ky => $itm) {
+
                 if ($key == $ky) {
                     continue;
                 }
                 if ($item == $itm) {
-                    $sndUsr[$item->id] = '';
+                    $sndUsr[$key] = '';
                 } //end of if
-            } //end of forech itm
+            } //end of foreach itm
         } //end of foreach item
-        $sndUsr = array_filter($sndUsr);
+        // dd($sndUsr);
+        $sndUsr = array_reverse(array_filter($sndUsr));
+
         //  dd($sndUsr);
         return view('message.messages', compact('sndUsr'));
     } //end of messages_users
 
 
     public function message_user($username){
-        return view('message.my_you');
+
+        $usr_id=User::where('username',$username)->first();
+        $recive_usr=ReciveMessage::where('user_id',$usr_id->id)->get();
+        $all_msg=[];
+        //this section for meesage reciveing by username
+        foreach($recive_usr as $key=>$item){
+            $snd_usr=User::find(Message::find($item->message_id)->user_id);
+            if($snd_usr ==auth()->user()){
+                $all_msg[$key]=Message::where('id',$item->message_id)->get();
+            }//end of if
+        }//end of foreach
+
+        //this section for meesage sending from username to me
+         $recive_usr=ReciveMessage::where('user_id',auth()->user()->id)->get();
+         $count=count($all_msg);
+         foreach($recive_usr as $key=>$item){
+            $snd_usr=User::find(Message::find($item->message_id)->user_id);
+
+            if($snd_usr ==$usr_id){
+                $all_msg[$count ++]=Message::where('id',$item->message_id)->get();
+            }//end of if
+        }//end of foreach
+
+        //for sort by created_at
+
+        //dd($all_msg,$all_msg[1]);
+          $all_msg=(object)array_reverse($all_msg);;
+
+
+        return view('message.my_you',compact('all_msg','usr_id'));
     }//end of message_user
 }
---
+++ resources/views/message/my_you.blade.php
@@ -0,0 +1,121 @@
+@extends('layouts.base')
+@section('style')
+    <link rel="stylesheet" href="{{ asset('css/mySass/my_you/style.css') }}" />
+@stop
+@section('title')
+    {{ $usr_id->username }}
+@stop
+
+@section('body')
+    <div class="background_image">
+        <img src="{{ asset('media/1.jpg') }}" alt="" />
+    </div>
+    <!-- consignee -->
+    <div class="consignee">
+        <!-- contente-users -->
+        <div class="contente-users">
+            <i class="fas fa-arrow-left"></i>
+            <div>
+                <img src="{{ $usr_id->img }}" alt="" />
+                <div>
+                    <p>{{ $usr_id->username }}</p>
+                </div>
+            </div>
+        </div>
+        <!-- contente-users -->
+        <!-- body of message -->
+        <div class="message-cover">
+            @foreach ($all_msg as $msgs)
+                @foreach ($msgs as $msg)
+                    <input type="hidden" name="msg" value="{{ $msg }}">
+                @endforeach
+            @endforeach
+
+        </div>
+        <!-- //body of message -->
+
+        <!-- form message -->
+        <div class="form-message">
+            <form action="" id="data" method="post">
+                <input type="text" name="message">
+                <button id="send"><i class="fas fa-paper-plane"></i></button>
+            </form>
+        </div>
+        <!-- //form message -->
+    </div>
+    <!-- //consignee -->
+@stop
+
+@section('script')
+    <script>
+        var jsn = '';
+        var obj2 = [];
+        var usrId = parseInt("{{ auth()->user()->id }}");
+        var recivephoto = "{{ $usr_id->img }}"
+        var obj = document.querySelectorAll('input[name=msg]')
+
+        //convert string to json
+        for (i = 0; i < obj.length; i++) {
+            jsn = JSON.parse(obj[i].value);
+            obj2[i] = jsn;
+            jsn = '';
+        } //end of for i
+        //end convert to string
+
+
+        //sort object
+        newobj = obj2.sort(function(a, b) {
+            return new Date(a.created_at) - new Date(b.created_at);
+        })
+        //end of sort
+
+        //give the messages to html code
+        var bdy = document.body;
+        for (var i = 0; i < newobj.length; i++) {
+            if (obj2[i].user_id == usrId) {
+                console.log('true')
+                var myMessage = document.createElement('div');
+                myMessage.classList = 'my-message';
+
+                var createP = document.createElement('p');
+                createP.textContent = newobj[i].content;
+                myMessage.appendChild(createP);
+
+                var messageCover = document.getElementsByClassName('message-cover')[0];
+                messageCover.appendChild(myMessage);
+            } else {
+                var yourMessage = document.createElement('div');
+                yourMessage.classList = 'your-message';
+                var youPhoto = document.createElement('div');
+                youPhoto.classList = 'your-photo';
+                var photo = document.createElement('img')
+                photo.src = recivephoto;
+                youPhoto.appendChild(photo)
+                yourMessage.appendChild(youPhoto);
+
+                var contentMessage = document.createElement('div');
+                contentMessage.classList = 'content-your-message';
+                var createP = document.createElement('p');
+                createP.textContent = newobj[i].content;
+                contentMessage.appendChild(createP);
+
+                var messageCover = document.getElementsByClassName('message-cover')[0];
+                messageCover.appendChild(yourMessage);
+                yourMessage.appendChild(contentMessage)
+                messageCover.appendChild(yourMessage);
+
+            } //end of if
+        } //end of for i
+
+        //end give the messages to html code
+    </script>
+
+    <script>
+        //make scrollbar in bottom
+
+        var messageCover = document.getElementsByClassName('message-cover')[0];
+        messageCover.scrollTop=messageCover.scrollHeight;
+
+        //end make scrollbar in bottom
+    </script>
+@stop
