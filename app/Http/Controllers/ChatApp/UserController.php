0744e1242fa498199af05780b5239173cb066ac6
ammaralazie <alaziiammar@gmail.com>
2021-07-31 18:18:45 +0300

    code

--- app/Http/Controllers/ChatApp/UserController.php
+++ app/Http/Controllers/ChatApp/UserController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index(Request $request){
-        
+        return response()->json(['msg'=>'welocm in home page']);
     }//end of index
 }
