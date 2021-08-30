ba953860fc3bb48d10e8366f665d372c6814e147
ammaralazie <alaziiammar@gmail.com>
2021-08-30 04:05:00 +0300

    code

--- app/Http/Controllers/ChatApp/UserController.php
+++ app/Http/Controllers/ChatApp/UserController.php
@@ -44,6 +44,7 @@ class UserController extends Controller
     public function signup(Request $request)
     {
 
+        //this from prch-cht
         $vldate = Validator::make($request->all(), [
             "username" => ['required', 'string', 'unique:users,username'],
             "email" => ['required', 'email', 'unique:users,email'],
